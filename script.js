fd4eacfb4634ea33451c9288aa71621494e91325
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-07-23 17:06:25 +0200

    more game logic

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 //selecting elements
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const score0 = document.querySelector('#score--0');
 //a little bit faster
 const score1 = document.getElementById('score--1');
@@ -16,7 +18,9 @@ score1.textContent = 0;
 //add + STRING
 dice.classList.add('hidden');
 
+const finalScores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 //rolling dice process
 btnRoll.addEventListener('click', function () {
@@ -29,8 +33,28 @@ btnRoll.addEventListener('click', function () {
   if (diceRoll !== 1) {
     //add to current score
     currentScore += diceRoll;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
     current0.textContent = currentScore;
   } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
     //switch to next player
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    //resent current score
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // add score to the active player
+  finalScores[activePlayer] += currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    finalScores[activePlayer];
+  //check if score =>100
+  //finish game
+
+  //switch to the 2nd player
 });
