5d90b3df90964e380abb3e8a704505f7f7ac464d
becauseidontlike <120679852+becauseidontlike@users.noreply.github.com>
2023-08-13 15:45:13 +0200

    winning execution

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ dice.classList.add('hidden');
 const finalScores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -34,32 +35,42 @@ const switchPlayer = function () {
 
 //rolling dice process
 btnRoll.addEventListener('click', function () {
-  //getting random roll
-  const diceRoll = Math.trunc(Math.random() * 6) + 1;
-  // display of the dice
-  dice.classList.remove('hidden');
-  dice.src = `dice-${diceRoll}.png`;
-  //if 1 - switch to second player
-  if (diceRoll !== 1) {
-    //add to current score
-    currentScore += diceRoll;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
+  if (playing) {
+    //getting random roll
+    const diceRoll = Math.trunc(Math.random() * 6) + 1;
+    // display of the dice
+    dice.classList.remove('hidden');
+    dice.src = `dice-${diceRoll}.png`;
+    //if 1 - switch to second player
+    if (diceRoll !== 1) {
+      //add to current score
+      currentScore += diceRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
 
-    current0.textContent = currentScore;
-  } else {
-    switchPlayer();
+      current0.textContent = currentScore;
+    } else {
+      switchPlayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  // add score to the active player
-  finalScores[activePlayer] += currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    finalScores[activePlayer];
-  //check if score =>100
-  //finish game
+  if (playing) {
+    finalScores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      finalScores[activePlayer];
 
-  //switch to the 2nd player
-  switchPlayer();
+    if (finalScores[activePlayer] >= 100) {
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      switchPlayer();
+    }
+  }
 });
